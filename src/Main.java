4fb82e1eb0a871ec21e6ffa25988ba32d7ea4321
André Luiz <andre.doctors@gmail.com>
2023-02-18 15:47:43 -0300

    desenvolvimento class Curso, mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import br.com.plataformacurso.dominio.Curso;
+import br.com.plataformacurso.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso = new Curso();
+        Mentoria mentoria = new Mentoria();
+
+        curso.setTitulo("Curso Java");
+        curso.setDescricao("Descrição do curso");
+        curso.setCargaHoraria(80);
+
+        System.out.println(curso);
+        System.out.println();
+
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição da mentoria");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+
     }
 }
