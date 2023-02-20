61614970788f4fd67167f2e508d96badebebace6
André Luiz <andre.doctors@gmail.com>
2023-02-20 10:39:48 -0300

    Finalizando a versao 1 do projeto

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import br.com.plataformacurso.dominio.Aluno;
+import br.com.plataformacurso.dominio.Bootcamp;
 import br.com.plataformacurso.dominio.Curso;
 import br.com.plataformacurso.dominio.Mentoria;
 
@@ -12,14 +14,36 @@ public class Main {
         curso.setDescricao("Descrição do curso");
         curso.setCargaHoraria(80);
 
+        /*
         System.out.println(curso);
         System.out.println();
+         */
 
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Descrição da mentoria");
         mentoria.setData(LocalDate.now());
 
+        /*
         System.out.println(mentoria);
+        System.out.println();
+        */
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescrição("Descrição do bootcamp");
+        bootcamp.getConteudo().add(curso);
+        bootcamp.getConteudo().add(mentoria);
+
+        Aluno aluno = new Aluno();
+        aluno.setNome("André Luiz");
+        aluno.inscreverBootcamp(bootcamp);
+
+        System.out.println(aluno.getNome());
+        System.out.println("Conteudos inscritos: " + aluno.getConteudosInscritos());
+        aluno.progredir();
+        System.out.println("Conteudos Concluidos: " + aluno.getConteudosConcluidos());
+        System.out.println("Conteudos inscritos: " + aluno.getConteudosInscritos());
+        System.out.println("XP: "+ aluno.calcularTotalXp());
 
     }
 }
